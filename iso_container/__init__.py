ac417535b2f9d5e03d3e57fe4e43bc7d0097b590
Jiseoup <jseoup@gmail.com>
2026-07-14 01:02:16 +0900

    chore: bump version to 1.1.0

--- iso_container/__init__.py
+++ iso_container/__init__.py
@@ -2,6 +2,6 @@ from .iso_container import get_container_info, validate_container
 
 __all__ = ['get_container_info', 'validate_container']
 
-__version__ = '1.0.3'
-__author__ = 'Jiseoup Lim'
+__version__ = '1.1.0'
+__author__ = 'JISUB LIM'
 __email__ = 'jseoup@gmail.com'
